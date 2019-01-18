5221ea3c1cc5e9042d53108a71af498ec492e1f7
Hugo Kindel <kindelhugo@gmail.com>
2019-01-18 23:09:11 +0100

    Update

--- Sources/Aryiele/Core/Logger.h
+++ Sources/Aryiele/Core/Logger.h
@@ -3,48 +3,20 @@
 
 #include <Vanir/Logger.h>
 
-#if defined(CONFIGURATION_DEBUG)
-#define LOG_TEXT(LOGTEXT, ...) \
-{ \
-std::string _aesir_log_text__ = ::Vanir::Logger::Log(LOGTEXT, __FUNCTION__, __LINE__, nbarg(__VA_ARGS__), ##__VA_ARGS__); \
-}
-#define LOG_INFO(LOGTEXT, ...) \
-{ \
-    std::string _aesir_log_text__ = ::Vanir::Logger::LogInfo(LOGTEXT, __FUNCTION__, __LINE__, nbarg(__VA_ARGS__), ##__VA_ARGS__); \
-}
-#define LOG_WARNING(LOGTEXT, ...) \
-{ \
-    std::string _aesir_log_text__ = ::Vanir::Logger::LogWarning(LOGTEXT, __FUNCTION__, __LINE__, nbarg(__VA_ARGS__), ##__VA_ARGS__); \
-}
-#define LOG_ERROR(LOGTEXT, ...) \
-{ \
-    std::string _aesir_log_text__ = ::Vanir::Logger::LogError(LOGTEXT, __FUNCTION__, __LINE__, nbarg(__VA_ARGS__), ##__VA_ARGS__); \
-}
+#define LOG(...) VANIR_LOG(__VA_ARGS__)
+#define LOG_INFO(...) VANIR_LOG_INFO(__VA_ARGS__)
+#define LOG_WARNING(...) VANIR_LOG_WARNING(__VA_ARGS__)
+#define LOG_ERROR(...) VANIR_LOG_ERROR(__VA_ARGS__)
+#ifdef PLATFORM_WINDOWS
+#define ULOG(...) VANIR_ULOG(__VA_ARGS__)
+#define ULOG_INFO(...) VANIR_ULOG_INFO(__VA_ARGS__)
+#define ULOG_WARNING(...)VANIR_ULOG_WARNING(__VA_ARGS__)
+#define ULOG_ERROR(...) VANIR_ULOG_ERROR(__VA_ARGS__)
 #else
-#define LOG_TEXT(LOGTEXT, ...) \
-{ \
-    std::string _aesir_log_text__ = ::Vanir::Logger::Log(LOGTEXT, nbarg(__VA_ARGS__), ##__VA_ARGS__); \
-}
-#define LOG_INFO(LOGTEXT, ...) \
-{ \
-    std::string _aesir_log_text__ = ::Vanir::Logger::LogInfo(LOGTEXT, nbarg(__VA_ARGS__), ##__VA_ARGS__); \
-}
-#define LOG_WARNING(LOGTEXT, ...) \
-{ \
-    std::string _aesir_log_text__ = ::Vanir::Logger::LogWarning(LOGTEXT, nbarg(__VA_ARGS__), ##__VA_ARGS__); \
-}
-#define LOG_ERROR(LOGTEXT, ...) \
-{ \
-    std::string _aesir_log_text__ = ::Vanir::Logger::LogError(LOGTEXT, nbarg(__VA_ARGS__), ##__VA_ARGS__); \
-}
+#define ULOG(...) VANIR_LOG(__VA_ARGS__)
+#define ULOG_INFO(...) VANIR_LOG_INFO(__VA_ARGS__)
+#define ULOG_WARNING(...) VANIR_LOG_WARNING(__VA_ARGS__)
+#define ULOG_ERROR(...) VANIR_LOG_ERROR(__VA_ARGS__)
 #endif
-#define LOG_DEFAULT(LOGTEXT, ...) \
-{ \
-    std::string _aesir_log_text__ = ::Vanir::Logger::LogDefault(LOGTEXT, nbarg(__VA_ARGS__), ##__VA_ARGS__); \
-}
-#define LOG_USER(LOGTEXT, ...) \
-{ \
-    std::string _aesir_log_text__ = ::Vanir::Logger::LogDefault(LOGTEXT, nbarg(__VA_ARGS__), ##__VA_ARGS__); \
-}
 
 #endif /* ARYIELE_LOGGER_H. */
